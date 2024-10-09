dddee3e2db4e18f9463fe1038da5287d9773d0fe
Isaac Gemal <isaacgemal@gmail.com>
2024-10-09 13:49:52 -0400

    Add button to storybook

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function MyButton() {
+function MyButton({ text = "Button" }: { text: string }) {
     return (
         <div className="relative inline-block">
             {/* Button content */}
             <button className="relative z-10 py-2 px-6 font-bold text-white bg-neonSunset transform skew-x-[30deg] hover:bg-electricDream hover:shadow-lg transition-all">
-                <span className="inline-block transform -skew-x-[30deg]">Button</span>
+                <span className="inline-block transform -skew-x-[30deg]">{text}</span>
             </button>
         </div>
     );
